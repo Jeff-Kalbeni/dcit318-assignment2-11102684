1a02daaa0cd05bfd2926f5b5755c54bdc3fa01e7
USER <jeffkalbeni@gmail.com>
2025-08-05 18:56:44 +0000

    Interfaces application

---
+++ AssignmentTwo/Interfaces/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        interface IMovable
+        {
+            void Move();
+        }
+
+        class Car : IMovable
+        {
+            public  void Move()
+            {
+                Console.WriteLine("Car is moving");
+            }
+        }
+
+        class Bicycle : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Bicycle is moving");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
