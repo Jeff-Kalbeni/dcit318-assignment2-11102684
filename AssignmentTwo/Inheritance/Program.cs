7a4a89a24c5bf8e9cc850afd422369ee2d6df2c5
USER <jeffkalbeni@gmail.com>
2025-08-05 18:54:54 +0000

    Inheritance application

---
+++ AssignmentTwo/Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public virtual void MakeSound()
+            {
+                Console.WriteLine("Some generic sound");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+
+        class Cat : Animal
+        {
+            public override void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
