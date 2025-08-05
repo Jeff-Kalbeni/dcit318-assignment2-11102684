1b568a92d2d6a3f89563bffb9a8aeeab150db400
USER <jeffkalbeni@gmail.com>
2025-08-05 18:54:05 +0000

    Abstract class application

---
+++ AssignmentTwo/Abstract Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Abstract_Class
+{
+    internal class Program
+    {
+        abstract class Shape
+        {
+            public abstract void GetArea();
+        }
+
+        class Circle : Shape
+        {
+            public override void GetArea()
+            {
+                double area = 2 * Math.PI * (5 * 5);
+                Console.WriteLine("The area of the circle is: " + area);
+            }
+        }
+
+        class Rectangle : Shape
+        {
+            public override void GetArea()
+            {
+                double area = 20 * 10;
+                Console.WriteLine("The area of the rectangle is: " + area);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Rectangle rect = new Rectangle();
+
+            circle.GetArea();
+            rect.GetArea();
+        }
+    }
+}
